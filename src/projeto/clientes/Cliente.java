024412339eae416806c704fcdf39110cc465f074
Andre-wf <cl204161@g.unicamp.br>
2025-05-27 11:44:04 -0300

    sla

---
+++ src/projeto/clientes/Cliente.java
@@ -0,0 +1,186 @@
+package projeto.clientes;
+
+import projeto.IPessoa;
+import projeto.Pessoa;
+import projeto.Main;
+
+import static projeto.Main.kbd;
+
+public class Cliente extends Pessoa implements IPessoa {
+    //atributos
+    protected String cpf;
+    protected int compras;
+    protected boolean idoso;
+
+    public Cliente(){
+        super("", 0);
+        this.cpf = "";
+        this.compras = 0;
+        this.idoso = false;
+    }
+
+    public Cliente(String nome, int idade, String cpf, int compras, boolean idoso) {
+        super(nome, idade);
+        this.cpf = cpf;
+        this.compras = compras;
+        this.idoso = idoso;
+    }
+
+    //gets e sets
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getCompras() {
+        return compras;
+    }
+
+    public void setCompras(int compras) {
+        this.compras = compras;
+    }
+
+    public boolean isIdoso() {
+        return idoso;
+    }
+
+    public void setIdoso(boolean idoso) {
+        this.idoso = idoso;
+    }
+
+    //métodos
+    @Override
+    public void exibir() {
+        System.out.println(nome + ", " + idade + " anos, CPF: " + cpf + " N. de compras: " + compras + ", " + ((idoso)?"Idoso":"Jovem"));
+    }
+
+    public static Cliente cadastraCliente()
+    {
+        Cliente novoCliente = new Cliente();
+        System.out.print("\nNome do cliente> ");
+        Main.kbd.nextLine();
+        novoCliente.nome = kbd.nextLine();
+
+        System.out.print("Idade do cliente> ");
+
+        novoCliente.idade = kbd.nextInt();
+        kbd.nextLine(); // limpa buffer
+
+        System.out.print("Cpf do cliente> ");
+        novoCliente.setCpf(kbd.nextLine());
+
+        novoCliente.setIdoso(novoCliente.idade > 60);
+
+        novoCliente.exibir();
+
+        return novoCliente;
+    }
+
+    public static void excluirCliente(String clienteExcluir)
+    {
+        int token = 0; // verifica se um cliente foi excluído
+        for (Cliente P: Main.clientes)
+        {
+            if (P.nome.equals(clienteExcluir))
+            {
+                Main.clientesExcl.add(P);
+                Main.clientes.remove(P);
+                token = 1;
+                break;
+            }
+        }
+        if (token == 1) System.out.println("\nCliente excluído com sucesso.");
+        else System.out.println("\nNão há nenhum cliente com esse nome na lista.");
+    }
+
+    public static void procuraClienteInfo(String buscarPor) // Informações sobre um cliente
+    {
+        boolean token = false; //verifica se o cliente foi encontrado
+        for (Cliente P : Main.clientes)
+        {
+            if(P.nome.startsWith(buscarPor)) // Busca pelo nome do cliente
+            {
+                token = true;
+                System.out.print("\nCliente encontrado com sucesso> ");
+                P.exibir();
+                break;
+            }
+        }
+        if(!token) System.out.println("O cliente não foi encontrado.");
+    }
+
+    public static void procuraMaisvelho() // O cliente mais velho
+    {
+        Cliente maisVelho = Main.clientes.getFirst();
+        int max = maisVelho.getIdade();
+        for (Cliente P : Main.clientes) // Procura pelo cliente mais velho
+        {
+            if(P.getIdade() > max)
+            {
+                maisVelho = P;
+                max = P.getIdade();
+            }
+        }
+        System.out.print("\nO cliente mais velho é> ");
+        maisVelho.exibir();
+    }
+
+    public static void procuraMaisJovem() // O cliente mais jovem
+    {
+        Cliente maisJovem = Main.clientes.getFirst();
+        int min = maisJovem.getIdade();
+        for (Cliente P : Main.clientes) // Procura pelo cliente mais jovem
+        {
+            if(P.getIdade() < min)
+            {
+                maisJovem = P;
+                min = P.getIdade();
+            }
+        }
+        System.out.print("\nO cliente mais jovem é> ");
+        maisJovem.exibir();
+    }
+
+    public static void quantMaioresDe60() // Clientes maiores que 60 anos
+    {
+        int quantClientes = 0;
+        for(Cliente P : Main.clientes) if(P.getIdade() > 60) quantClientes += 1; // Procura por clientes maiores de 60
+
+        if(quantClientes != 0) System.out.println("\nA quantidade de clientes maiores de 60 anos é> " + quantClientes);
+        else System.out.println("\nNão há clientes com mais de 60 anos cadastrados.");
+    }
+
+    public static void quantMenoresde18() // Clientes menores que 18 anos
+    {
+        int quantClientes = 0;
+        for(Cliente P : Main.clientes) if(P.getIdade() < 18) quantClientes += 1; // Procura por clientes maiores de 60
+
+        if (quantClientes != 0) System.out.println("\nA quantidade de clientes menores de 18 anos é> " + quantClientes);
+        else System.out.println("\nNão há clientes com menos de 18 anos cadastrados.");
+    }
+
+    public static void quantMediaAnos()
+    {
+        int divide = 0, soma = 0;
+        for(Cliente P : Main.clientes)
+        {
+            soma += P.getIdade();
+            divide += 1;
+        };
+        System.out.println("\nA média das idades dos clientes é de> " + soma/divide);
+    }
+
+    public static void procuraExcluidos()
+    {
+        int aux = 1;
+        for (Cliente P : Main.clientesExcl)
+        {
+            System.out.printf("Cleinte excluído número %d> " + aux);
+            P.exibir();
+            aux ++;
+        }
+    }
+}
---
+++ src/projeto/produtos/Produto.java
@@ -0,0 +1,183 @@
+package projeto.produtos;
+
+import projeto.Main;
+import projeto.clientes.Cliente;
+import projeto.funcionarios.Funcionario;
+
+import static projeto.Main.kbd;
+
+public class Produto  {
+    protected double preco;
+    protected String descricao;
+    protected String modelo;
+    protected String marca;
+    protected int anolancamento;
+
+    //construtor
+
+    public Produto(double preco, String descricao, String modelo, String marca, int anolancamento) {
+        this.preco = preco;
+        this.descricao = descricao;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.anolancamento = anolancamento;
+    }
+
+    public Produto() {
+        this.preco = 0;
+        this.descricao = "";
+        this.modelo = "";
+        this.marca = "";
+        this.anolancamento = 0;
+    }
+
+    //gets e sets
+
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getanolancamento() {
+        return anolancamento;
+    }
+
+    public void setanoLancamento(int anoLancamento) {
+        this.anolancamento = anoLancamento;
+    }
+
+    //métodos
+    public void mostraInfo()
+    {
+        System.out.println("Modelo: " + modelo + ", Marca:" + marca + ", Preço: "+ preco + ", Lançado em: " + anolancamento + ", Descrição: " + descricao);
+    }
+
+
+    public static Produto cadastraProduto()
+    {
+        Produto novoProduto = new Produto();
+
+        System.out.print("\nDescrição do produto> "); //Descrição ****
+
+        kbd.nextLine(); // limpa buffer
+        novoProduto.setDescricao(kbd.nextLine()); // lê a descrição e atribui ao objeto produto
+
+        System.out.print("Modelo do produto> "); //Modelo ****
+        novoProduto.setModelo(kbd.nextLine()); // lê o modelo e atribui ao objeto produto
+
+        System.out.print("Preço do produto> "); //Preço ****
+
+        novoProduto.setPreco(kbd.nextInt()); //lê o preço e atribui ao objeto produto
+        kbd.nextLine(); // limpa buffer
+
+        System.out.print("Ano de lançamento do produto> "); //Ano de lançamento ****
+        novoProduto.setanoLancamento(kbd.nextInt());
+
+        System.out.print("Marca do produto> "); //Marca ****
+
+        kbd.nextLine(); // Limpa buffer
+        novoProduto.setMarca(kbd.nextLine());
+
+        System.out.println();
+        novoProduto.mostraInfo();
+
+        return novoProduto;
+    }
+
+    public static void procuraProdutoInfo(String decricao, String marca, String modelo)
+    {
+        boolean token = false; //verifica se o produto foi encontrado
+        int n = 0;
+        for (Produto P : Main.produtos)
+        {
+            if(P.getDescricao().startsWith(decricao) // Busca pela descrição do produto
+                    || P.getMarca().startsWith(marca) // Busca pela marcan do produto
+                    || P.getModelo().startsWith(modelo)) // Busca pelo modelo do produto
+            {
+                n += 1;
+                token = true;
+                System.out.printf("\nProduto encontrado (%d)> ", n);
+                P.mostraInfo();
+                System.out.println();
+            }
+        }
+        if(!token) System.out.println("O produto não foi encontrado.");
+    }
+
+    public static void procuraMenorPreco() // Produto com menor preço
+    {
+        Produto obj = Main.produtos.getFirst();
+
+        for (Produto P : Main.produtos) if (P.getPreco() < obj.getPreco()) obj = P;
+
+        System.out.println("\nO produto com o menor preço é> ");
+        obj.mostraInfo();
+    }
+
+    public static void procuraMaiorPreco()
+    {
+        Produto obj = Main.produtos.getFirst();
+
+        for (Produto P : Main.produtos) if (P.getPreco() > obj.getPreco()) obj = P;
+
+        System.out.println("\nO produto com o maior preço é> ");
+        obj.mostraInfo();
+    }
+
+    public static void procuraMediaPreco()
+    {
+        int divide = 0, soma = 0;
+        for(Produto P : Main.produtos)
+        {
+            soma += P.getPreco();
+            divide += 1;
+        };
+        System.out.println("\nA média dos preços dos produtos é de> " + soma/divide);
+    }
+
+    public static void procuraAcimadaMedia()
+    {
+        int divide = 0, soma = 0, quantAcima = 0;
+
+        for(Produto P : Main.produtos)
+        {
+            soma += P.getPreco();
+            divide += 1;
+        };
+
+        for(Produto P : Main.produtos)
+        {
+            if (soma/divide < P.getPreco()) quantAcima++;
+        }
+
+        System.out.print("\nA quantidade de produtos com preço acima da média é de> " + quantAcima);
+    }
+}
